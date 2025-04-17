2cd483616a4555d02a90b70b2cac2d09864cb9d2
Mweresa Esther Magoma <esthermweresa27@gmail.com>
2025-04-17 08:36:46 +0300

    final

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,10 @@
 // Your tests here
+import { pointsForward } from "../utils";
+
+describe("pointsForward", () => {
+    it("calculates the total points for a word (i point per vowel, 2 per consonant", () => {
+        const word = "test";
+        const points = pointsForward(word);
+        expect(points).toBe(7);
+    })
+})
